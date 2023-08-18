bf9bc79d007c4d0ec5ad7f6403f032d39a815603
Adeduro2 <adedurofolashade@gmail.com>
2023-08-17 17:52:57 -0700

    Comment out the part of code that is causing infinite loops

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -4,14 +4,15 @@
 
 /**
  *main - Entry point
+ *
  *Return: Always 0 (Success)
  */
 void positive_or_negative(int n)
 {
-	//int n;
+	int n;
 
-	//srand(time(0));
-	//n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -24,5 +25,5 @@ void positive_or_negative(int n)
 	{
 		printf("%d is negative\n", n);
 	}
-	return (0);
+	return(n);
 }
