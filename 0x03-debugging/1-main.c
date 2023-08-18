4719d6b732c5a5fa3ee2908edc27a8da3d45ed2c
Adeduro2 <adedurofolashade@gmail.com>
2023-08-18 16:32:19 -0700

    Fix the code in 2-largest_number.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - causes an infinite loop
 * Return: 0
@@ -11,14 +10,15 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	//i = 0;
+	/*
+	 * i = 0;
 
-	//while (i < 10)
+	* while (i < 10)
 	{
-		//putchar(i);
+		* putchar(i);
+	*/
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
