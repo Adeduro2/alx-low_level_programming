5ce5d29a60740f1b4c840f1569fb8e6a2f1e57b6
Adeduro2 <adedurofolashade@gmail.com>
2023-08-31 14:28:58 -0700

    a function that returns 1 if the input integer is a prime number, otherwise return 0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * primeFinder - searches for if a number is prime
+ *
+ * @i: the number to check
+ * @j: the numbers to go through
+ *
+ * Return: Whether or not the number is a prime number
+ */
+
+int primeFinder(int i, int j)
+{
+	if (i <= 1 || i % j == 0)
+		return (0);
+	else if (i == j)
+		return (1);
+	else if (i > j)
+		primeFinder(i, j + 1);
+
+	return (1);
+}
+
+/**
+ * is_prime_number - tells us if an integer is a prime number or not
+ *
+ * @n: the number to check
+ *
+ * Return: 0 if the number is not prime, and 1 if it is
+ */
+
+int is_prime_number(int n)
+{
+	return (primeFinder(n, 2));
+}
